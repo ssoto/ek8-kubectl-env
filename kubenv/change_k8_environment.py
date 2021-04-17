b7151d6e7a0d842d54da0bb170608b6c1d06c40e
Sergio Soto <sergiosnu@ext.inditex.com>
2021-04-17 15:06:53 +0200

    Enabled module code

---
+++ kubenv/change_k8_environment.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def change_paths():
+    print("Editable funcionando guay!!!")
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ssoto/ek8-kubectl-env",
     packages=setuptools.find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
