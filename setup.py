d0b102bb91a7c3a6cce88144c109703dcfea3a2d
Sergio Soto <sergiosnu@ext.inditex.com>
2021-04-17 14:41:58 +0200

    Fixed syntax error on setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
-    scripts=["bin/kubenv"]
+    ],
+    scripts=["bin/kubenv"],
 )
