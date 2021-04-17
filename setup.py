72eb38ea547ff2889ca210b17d169cac0b9e05f3
Sergio Soto <sergiosnu@ext.inditex.com>
2021-04-17 14:48:33 +0200

    Updated minor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kubenv",
-    version="0.0.2",
+    version="0.0.3",
     author="Sergio",
     author_email="scots4ever@gmail.com",
     description="Cool utilies'",
