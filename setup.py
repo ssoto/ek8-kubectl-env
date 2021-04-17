c0c90a5bdb3238e256a0eff15fff862f72a92016
Sergio Soto <sergiosnu@ext.inditex.com>
2021-04-17 14:50:42 +0200

    Added python_required to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ssoto/ek8-kubectl-env",
     packages=setuptools.find_packages(),
+    python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
